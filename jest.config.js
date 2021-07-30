de5fd63dbcaa8602b934b679277c71ec63e6c5f6
ajaypratap003 <ajay.pratap233@gmail.com>
2021-07-30 21:02:08 +0530

    remove enzyme and update cypress config

--- jest.config.js
+++ jest.config.js
@@ -35,6 +35,6 @@ module.exports = {
   testEnvironment: "jsdom",
 
   // A list of paths to snapshot serializer modules Jest should use for snapshot testing
-  snapshotSerializers: ['enzyme-to-json/serializer'],
+  //snapshotSerializers: ['enzyme-to-json/serializer'],
 
 };
--- test-setup.js
+++ test-setup.js
@@ -1,5 +1,1 @@
-import { configure } from '@testing-library/react';
-import Adapter from 'enzyme-adapter-react-16';
-import "@testing-library/jest-dom/extend-expect";
-
-configure({ adapter: new Adapter() });
+import '@testing-library/jest-dom';

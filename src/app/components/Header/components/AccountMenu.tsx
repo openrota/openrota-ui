9d3559fc1eadc3c896ed40ab80da648c3c13f7b8
manaswinidas <dasmanaswini10@gmail.com>
2022-04-12 16:17:46 +0530

    configure keycloak logout

--- src/app/components/Header/components/AccountMenu.tsx
+++ src/app/components/Header/components/AccountMenu.tsx
@@ -6,6 +6,7 @@ import MenuItem from '@mui/material/MenuItem';
 import Typography from '@mui/material/Typography';
 import { useTranslation } from 'react-i18next';
 import { useHistory } from 'react-router-dom';
+import { keycloak } from '@app/auth/keycloak';
 
 
 const AccountMenu = ({userName}) => {
@@ -20,6 +21,7 @@ const AccountMenu = ({userName}) => {
 
     const handleClose = (): void => {
         setAnchorEl(null);
+        keycloak?.logout();
     };
 
     const handleMyProfile = (): void => {
